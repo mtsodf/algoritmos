12c80162387444862e5387ffb2897989b3561408
Mateus Oliveira <mtsodf@gmail.com>
2023-08-09 20:08:58 -0300

    Implementação funcional do pnpoly

---
+++ Semana01/cli.py
@@ -0,0 +1,53 @@
+import argparse
+import matplotlib.pyplot as plt
+from inside_polygon import is_inside, read_polygon, is_inside_pnpoly
+
+args = argparse.ArgumentParser()
+args.add_argument("polygon_filename", help="Polygon filename")
+args.add_argument("--npoints", "-n", help="Number of grid points", type=int, default=10)
+args.add_argument("--invert_axis", "-i", help="Invert axis", action="store_true")
+args.add_argument("--outside", "-o", help="Plot outside points", action="store_true")
+
+args = args.parse_args()
+
+xs, ys = read_polygon(args.polygon_filename)
+
+fig, ax = plt.subplots(1,1)
+
+# Invert axis directions
+if args.invert_axis:
+    ax.invert_yaxis()
+
+
+xmin, xmax = min(xs), max(xs)
+ymin, ymax = min(ys), max(ys)
+
+inside_x = []
+inside_y = []
+outside_x = []
+outside_y = []
+NPOINTS = args.npoints
+
+for i in range(NPOINTS):
+    x = xmin + i*(xmax-xmin)/(NPOINTS -1)
+    for j in range(NPOINTS):
+        y = ymin + j*(ymax-ymin)/(NPOINTS -1)
+
+        if is_inside(xs, ys, x, y):
+            inside_x.append(x)
+            inside_y.append(y)
+        else:
+            outside_x.append(x)
+            outside_y.append(y)
+
+ax.scatter(inside_x, inside_y, color="blue")
+if args.outside:
+    ax.scatter(outside_x, outside_y, color="red")
+
+ax.plot(xs, ys)
+
+plt.show()
+
+
+
+
---
+++ Semana01/inside_polygon.py
@@ -0,0 +1,50 @@
+def check_intersect(xa, ya, xb, yb):
+    if ya > 0 == yb > 0:
+        return 0
+
+    xi = -ya*(xb - xa)/(yb - ya) + xa
+
+    if xi > 0:
+        return 1 if yb > ya else -1
+    return 0
+    
+
+def is_inside_pnpoly(xs, ys, x, y):
+    edge_count = len(xs) - 1
+
+    inside = False
+
+    for i in range(edge_count):
+        j = i + 1
+        a = (ys[i] > y) != (ys[j] > y)
+        b = False
+        if a:
+            b = x < (xs[j]-xs[i]) * (y-ys[i]) / (ys[j]-ys[i]) + xs[i] 
+        if a and b:
+            inside = not inside
+    return inside
+
+
+def is_inside(xs, ys, x, y):
+    edge_count = len(xs) - 1
+
+    ws = 0
+    for i in range(edge_count):
+        ws += check_intersect(xs[i]-x, ys[i]-y, xs[i+1]-x, ys[i+1]-y)
+
+    return ws % 2 == 1
+
+
+def read_polygon(filename, comma_separated=False):
+    xs = []
+    ys = []    
+    with open(filename) as f:
+        for line in f:
+            if "," in line:
+                x, y = [float(value) for value in line.split(",")]
+            else:
+                x, y = [float(value) for value in line.split()]
+            xs.append(x)
+            ys.append(y)
+    return xs, ys
+
