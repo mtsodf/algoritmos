00ba364a67a232394a60e4e66c55d8d4adc59704
Mateus Oliveira <mtsodf@gmail.com>
2023-08-11 07:25:08 -0300

    Adicionando opção para salvar as figuras ou executar para um ponto específico

--- Semana01/cli.py
+++ Semana01/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import matplotlib.pyplot as plt
 from inside_polygon import is_inside, read_polygon, is_inside_pnpoly
 
@@ -8,10 +9,14 @@ args.add_argument("--npoints", "-n", help="Number of grid points", type=int, def
 args.add_argument("--invert_axis", "-i", help="Invert axis", action="store_true")
 args.add_argument("--outside", "-o", help="Plot outside points", action="store_true")
 args.add_argument("--pnpoly", "-p", help="PNPOLY algorithm", action="store_true")
+args.add_argument("--point", "-P", help="Point to check", nargs=2, type=float, default=None)
+args.add_argument("--save", "-s", help="Save picture", action="store_true")
 
 args = args.parse_args()
 
 xs, ys = read_polygon(args.polygon_filename)
+
+print(f"Qtd Vertices = {len(xs) - 1}")
 
 fig, ax = plt.subplots(1,1)
 
@@ -21,34 +26,52 @@ if args.invert_axis:
 
 algorithm = is_inside_pnpoly if args.pnpoly else is_inside
 
-xmin, xmax = min(xs), max(xs)
-ymin, ymax = min(ys), max(ys)
+if args.point:
+    x, y = args.point
+    if algorithm(xs, ys, x, y):
+        print("Point ({}, {}) is inside".format(x, y))
+        c = "blue"
+    else:
+        print("Point ({}, {}) is outside".format(x, y))
+        c = "red"
+    
+    ax.scatter([x], [y], color=c)
 
-inside_x = []
-inside_y = []
-outside_x = []
-outside_y = []
+else:
+    xmin, xmax = min(xs), max(xs)
+    ymin, ymax = min(ys), max(ys)
 
-NPOINTS = args.npoints
-for i in range(NPOINTS):
-    x = xmin + i*(xmax-xmin)/(NPOINTS -1)
-    for j in range(NPOINTS):
-        y = ymin + j*(ymax-ymin)/(NPOINTS -1)
+    inside_x = []
+    inside_y = []
+    outside_x = []
+    outside_y = []
 
-        if algorithm(xs, ys, x, y):
-            inside_x.append(x)
-            inside_y.append(y)
-        else:
-            outside_x.append(x)
-            outside_y.append(y)
+    NPOINTS = args.npoints
+    for i in range(NPOINTS):
+        x = xmin + i*(xmax-xmin)/(NPOINTS -1)
+        for j in range(NPOINTS):
+            y = ymin + j*(ymax-ymin)/(NPOINTS -1)
 
-ax.scatter(inside_x, inside_y, color="blue")
-if args.outside:
-    ax.scatter(outside_x, outside_y, color="red")
+            if algorithm(xs, ys, x, y):
+                inside_x.append(x)
+                inside_y.append(y)
+            else:
+                outside_x.append(x)
+                outside_y.append(y)
+
+    ax.scatter(inside_x, inside_y, color="blue")
+    if args.outside:
+        ax.scatter(outside_x, outside_y, color="red")
 
 ax.plot(xs, ys, color="black")
+ax.set_title(os.path.basename(args.polygon_filename))
 
-plt.show()
+if args.save:
+    figure_path = os.path.splitext(args.polygon_filename)[0] + ".png"
+    print(f"Saving file {figure_path}")
+    plt.savefig(figure_path)
+else:
+    plt.show()
 
 
 
