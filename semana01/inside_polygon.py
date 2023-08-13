a6c3d00fee8324d144505a729c9824b9de5b413a
Mateus Oliveira <mtsodf@gmail.com>
2023-08-12 21:12:05 -0300

    Modificações no algoritmo para retornar as interseções

--- semana01/inside_polygon.py
+++ semana01/inside_polygon.py
@@ -1,12 +1,13 @@
-def check_intersect(xa, ya, xb, yb):
-    if (ya > 0) == (yb > 0):
-        return 0
+def check_intersect(xp, yp, xa, ya, xb, yb):
+    if (ya > yp and yb > yp) or (ya <= yp and yb <= yp):
+        return None
 
-    xi = -ya*(xb - xa)/(yb - ya) + xa
+    xi = (yp-ya)*(xb - xa)/(yb - ya) + xa
 
-    if xi > 0:
-        return 1 if yb > ya else -1
-    return 0
+    if xi > xp:
+        return xi
+    else:
+        return None
     
 
 def is_inside_pnpoly(xs, ys, x, y):
@@ -23,17 +24,24 @@ def is_inside_pnpoly(xs, ys, x, y):
     return inside
 
 
-def is_inside(xs, ys, x, y):
+def is_inside(xs, ys, x, y, return_intersects=False):
     edge_count = len(xs) - 1
-
+    intersects = []
     ws = 0
     for i in range(edge_count):
-        ws += check_intersect(xs[i]-x, ys[i]-y, xs[i+1]-x, ys[i+1]-y)
+        intersection = check_intersect(x, y, xs[i], ys[i], xs[i+1], ys[i+1]) 
+        if intersection is not None:
+            ws += 1
+            if return_intersects:
+                intersects.append(i)
 
+    if return_intersects:
+        return ws % 2 == 1, intersects
+    
     return ws % 2 == 1
 
 
-def read_polygon(filename, comma_separated=False):
+def read_polygon(filename, comma_separated=False, invert_x_y=False):
     xs = []
     ys = []    
     with open(filename) as f:
@@ -44,8 +52,13 @@ def read_polygon(filename, comma_separated=False):
                 x, y = [float(value) for value in line.split()]
             xs.append(x)
             ys.append(y)
-    if xs[0] != xs[-1] and ys[0] != ys[-1]:
+
+    if xs[0] != xs[-1] or ys[0] != ys[-1]:
         xs.append(xs[0])
         ys.append(ys[0])
-    return xs, ys
+
+    if invert_x_y: 
+        return ys, xs
+    else:
+        return xs, ys
 
