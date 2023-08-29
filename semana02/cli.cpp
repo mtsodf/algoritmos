fd55daf3faa569b027d727b2f11a4cf43ee37285
Mateus Oliveira <mtsodf@gmail.com>
2023-08-29 00:09:43 -0300

    Contar tempo com cli.cpp

--- semana02/cli.cpp
+++ semana02/cli.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <fstream>
 #include "polygon.h"
 
@@ -10,12 +11,18 @@ int main(int argc, char const *argv[])
 
     Polygon *p = new Polygon(&xs, &ys);
 
+    // Measure time
+    clock_t begin = clock();
     vector<Polygon> *triangles = p->triangulate();
+    clock_t end = clock();
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 
-    for (int i = 0; i < triangles->size(); i++)
-    {
-        print_polygon(&(*triangles)[i]);
-    }
+    cout << argv[1] << ";" << p->vert_list->size() << ";" << elapsed_secs << endl;
+
+    // for (int i = 0; i < triangles->size(); i++)
+    // {
+    // print_polygon(&(*triangles)[i]);
+    // }
 
     return 0;
 }
