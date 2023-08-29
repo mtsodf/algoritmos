b419594fd2304001163caae678b15396e1803e3e
Mateus Oliveira <mtsodf@gmail.com>
2023-08-29 11:14:00 -0300

    Melhoria do cli da triangulação

--- semana02/cli.py
+++ semana02/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import os
 import matplotlib.pyplot as plt
 from triangulate import triangulate, read_polygon, Polygon
 
@@ -8,8 +9,12 @@ sys.setrecursionlimit(10**5)
 
 # Parse arguments
 parser = argparse.ArgumentParser(description="Triangulate a polygon")
+parser.add_argument("input", type=str, help="Input file")
+parser.add_argument("--show", "-s", action="store_true", help="Show plot")
+parser.add_argument("--polygononly", "-p", action="store_true", help="Show plot")
+parser.add_argument("--outputdir", "-o", type=str, default="./", help="Ouput directory")
 parser.add_argument(
-    "--input", type=str, help="Input file", default="./semana02/data/asadelta.txt"
+    "--suffix", "-x", type=str, default="", help="Suffix for output file"
 )
 
 args = parser.parse_args()
@@ -18,14 +23,19 @@ args = parser.parse_args()
 xs, ys = read_polygon(args.input, close_polygon=False)
 polygon = Polygon(xs, ys)
 
-# Triangulate
-triangs = triangulate(polygon)
 
 fig, ax = plt.subplots(1, 1)
 
-triangs = triangulate(polygon)
-for triang in triangs:
-    triang.plot(ax=ax, color="red")
+if not args.polygononly:
+    # Triangulate
+    triangs = triangulate(polygon)
+    for triang in triangs:
+        triang.plot(ax=ax, color="red")
 
-polygon.plot(ax=ax, linestyle="--")
-plt.show()
+polygon.plot(ax=ax, color="black")
+if args.show:
+    plt.show()
+
+pngfile = os.path.splitext(args.input)[0] + args.suffix + ".png"
+pngfile = os.path.join(args.outputdir, os.path.basename(pngfile))
+plt.savefig(pngfile)
