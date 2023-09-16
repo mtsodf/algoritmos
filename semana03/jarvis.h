f445eda3f3cbcd911a4661eb623587813cba0eba
Mateus Oliveira <mtsodf@gmail.com>
2023-09-16 20:57:15 -0300

    Expondo função de cálculo de ângulos do jarvis

--- semana03/jarvis.h
+++ semana03/jarvis.h
@@ -4,5 +4,6 @@
 using namespace std;
 
 bool ccw(double xa, double ya, double xb, double yb, double xc, double yc);
+double angle(double xa, double ya, double xb, double yb);
 vector<int> *jarvis(vector<double> xs, vector<double> ys);
 vector<int> *paper_jarvis(vector<double> xs, vector<double> ys);
