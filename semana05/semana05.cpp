35ab920af565a60bde8a04b8483ed2c7a0c08d5f
Mateus Oliveira <mtsodf@gmail.com>
2023-10-01 14:19:00 -0300

    Adicionando argumentos de linha de comando para semana05

--- semana05/semana05.cpp
+++ semana05/semana05.cpp
@@ -1,3 +1,4 @@
+#include <boost/program_options.hpp>
 #include <iostream>
 
 #include "points_triangulation.h"
@@ -5,18 +6,33 @@
 #include "random_points.h"
 #include "utilities.h"
 
-int main() {
-    vector<double> xs;
-    vector<double> ys;
-    // read_polygon_from_file("../../semana03/data/alien_no_duplicates.txt", xs, ys, false);
-    // read_polygon_from_file("../../semana02/data/spiral/spiral_100.txt", xs, ys, false);
-    generate_regular_polygon(10, xs, ys);
-    // xs.push_back(0.1);
-    // ys.push_back(0);
+namespace po = boost::program_options;
 
-    for (int i = 0; i < 10; i++) {
-        xs.push_back((double)rand() / RAND_MAX);
-        ys.push_back((double)rand() / RAND_MAX);
+int main(int argc, char const *argv[]) {
+    po::options_description desc("Allowed options");
+    desc.add_options()("help", "produce help message")("input", po::value<string>(), "Input points")("random_points", po::value<int>(), "Generate random points");
+    desc.add_options()("output", po::value<string>()->default_value("triangulation.json"), "Output file");
+    desc.add_options()("alg", po::value<string>()->default_value("std"), "Algorithm to sort points");
+    desc.add_options()("circle", po::value<int>()->default_value(0), "Generate points in a circle");
+    desc.add_options()("comma", po::value<int>()->default_value(0), "File with comma separated points");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    string output_filepath = vm["output"].as<string>();
+
+    vector<double> xs, ys;
+    if (vm.count("input")) {
+        string input_file = vm["input"].as<string>();
+        cout << "Input file: " << input_file << "\n";
+        read_polygon_from_file(input_file, xs, ys, vm["comma"].as<int>() != 0);
+    } else if (vm.count("random_points")) {
+        int n = vm["random_points"].as<int>();
+        generate_random_points(n, xs, ys, vm["circle"].as<int>());
+    } else {
+        std::cout << "No input file was given\n";
+        return 1;
     }
 
     vector<Point *> *points = new vector<Point *>;
@@ -29,5 +45,5 @@ int main() {
     vector<vector<int>> adjacency_list;
     triangulate_trig(points, triangles, adjacency_list);
 
-    write_triangulation_json(points, &triangles, &adjacency_list, "triangulation.json");
+    write_triangulation_json(points, &triangles, &adjacency_list, output_filepath);
 }
