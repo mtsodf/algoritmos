3f8373003b0e95fb59661f847e8193e0ec978205
Mateus Oliveira <mtsodf@gmail.com>
2023-10-08 15:26:03 -0300

    Mais parâmetros de configuração do script

--- semana06/semana06.cpp
+++ semana06/semana06.cpp
@@ -12,21 +12,25 @@ int main(int argc, char const *argv[]) {
     po::options_description desc("Allowed options");
     desc.add_options()("help", "produce help message")("random_points", po::value<int>(), "Generate random points");
     desc.add_options()("output", po::value<string>()->default_value("output.json"), "Output file");
+    desc.add_options()("event", po::value<string>()->default_value(""), "Output event file");
     desc.add_options()("alg", po::value<string>()->default_value("std"), "Algorithm to sort points");
     desc.add_options()("circle", po::value<int>()->default_value(0), "Generate points in a circle");
     desc.add_options()("comma", po::value<int>()->default_value(0), "File with comma separated points");
+    desc.add_options()("length", po::value<double>()->default_value(0.1), "Mean of segment lenghts");
+    desc.add_options()("length_std", po::value<double>()->default_value(0.01), "Mean of segment lenghts");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
 
     string output_filepath = vm["output"].as<string>();
+    string event_filepath = vm["event"].as<string>();
 
     vector<Segment *> segments;
+    int n = -1;
     if (vm.count("random_points")) {
-        int n = vm["random_points"].as<int>();
-        generate_segments(n, 0.5, 0.05, segments);
-
+        n = vm["random_points"].as<int>();
+        generate_segments(n, vm["length"].as<double>(), vm["length_std"].as<double>(), segments);
     } else {
         std::cout << "No input file was given\n";
         return 1;
@@ -51,23 +55,25 @@ int main(int argc, char const *argv[]) {
     json_file.open(output_filepath, ios::out);
     json_file << "{\n";
     json_file << "\"time\":" << std::scientific << std::setprecision(8) << cpu_time_used << ",\n";
-    json_file << "  \"segments\": [\n";
-    for (int i = 0; i < segments.size(); i++) {
-        json_file << "    [" << segments[i]->start->x << ", " << segments[i]->start->y << ", " << segments[i]->end->x << ", " << segments[i]->end->y << "]";
-        if (i < segments.size() - 1) {
-            json_file << ",";
+    if (n < 10000) {
+        json_file << "  \"segments\": [\n";
+        for (int i = 0; i < segments.size(); i++) {
+            json_file << "    [" << segments[i]->start->x << ", " << segments[i]->start->y << ", " << segments[i]->end->x << ", " << segments[i]->end->y << "]";
+            if (i < segments.size() - 1) {
+                json_file << ",";
+            }
+            json_file << "\n";
         }
-        json_file << "\n";
-    }
-    json_file << "  ],\n";
-    json_file << "  \"intersections\": [\n";
-    for (int i = 0; i < intersections.size(); i++) {
-        json_file << "    [" << intersections[i].first << ", " << intersections[i].second << "]";
-        if (i < intersections.size() - 1) {
-            json_file << ",";
+        json_file << "  ],\n";
+        json_file << "  \"intersections\": [\n";
+        for (int i = 0; i < intersections.size(); i++) {
+            json_file << "    [" << intersections[i].first << ", " << intersections[i].second << "]";
+            if (i < intersections.size() - 1) {
+                json_file << ",";
+            }
+            json_file << "\n";
         }
-        json_file << "\n";
+        json_file << "  ]\n";
     }
-    json_file << "  ]\n";
     json_file << "}\n";
 }
