8e230f751f474cc804f7335cc4392cebed4bea91
Mateus Oliveira <mtsodf@gmail.com>
2023-10-15 14:55:44 -0300

    Melhoria no cálculo da intersecção vertical

--- semana06/segment.cpp
+++ semana06/segment.cpp
@@ -2,10 +2,31 @@
 
 #include "point.h"
 void Segment::calc_intersection(const Segment &other, double &x, double &y) {
-    double a0 = (this->end->y - this->start->y) / (this->end->x - this->start->x);
+    double dx0 = this->end->x - this->start->x;
+    double dx1 = other.end->x - other.start->x;
+    double dy0 = this->end->y - this->start->y;
+    double dy1 = other.end->y - other.start->y;
+
+    if (dx0 == 0) {
+        x = this->start->x;
+        double a1 = dy1 / dx1;
+        double b1 = -a1 * other.start->x + other.start->y;
+        y = a1 * x + b1;
+        return;
+    }
+    if (dx1 == 0) {
+        x = other.start->x;
+        double a0 = dy0 / dx0;
+        double b0 = -a0 * this->start->x + this->start->y;
+        y = a0 * x + b0;
+        return;
+    }
+
+    double a0 = dy0 / dx0;
     double b0 = -a0 * this->start->x + this->start->y;
-    double a1 = (other.end->y - other.start->y) / (other.end->x - other.start->x);
+    double a1 = dy1 / dx1;
     double b1 = -a1 * other.start->x + other.start->y;
+
     x = (b1 - b0) / (a0 - a1);
     y = a0 * x + b0;
 }
