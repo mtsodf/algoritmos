0c5170658f2b1e4bd6e30df4daff78610696600d
mtsodf <mateus.figueiredo@petrobras.com.br>
2023-10-18 16:00:50 -0300

    Trocando default quando gerar o grid

--- semana06/intersection_detection.h
+++ semana06/intersection_detection.h
@@ -18,7 +18,7 @@ bool segment_intersection(vector<Point *> &start, vector<Point *> &end, vector<p
 bool segment_intersection(vector<Segment *> &segments, vector<pair<int, int>> &intersection_pairs, vector<int> *active_segments_size, const string &segment_container_type, const string &event_container_type = "heap", const string &events_filename = "", bool detection = true);
 
 void generate_segments(int n, double length_mean, double length_std, vector<Segment *> &segments);
-void generate_grid(int n, vector<Segment *> &segments, bool random_start = true);
+void generate_grid(int n, vector<Segment *> &segments, bool random_start = false);
 void generate_vert_grid(int nx, int ny, vector<Segment *> &segments);
 void generate_segments_no_intersect(int n, vector<Segment *> &segments);
 void generate_big_segments_no_intersect(int n, vector<Segment *> &segments);
